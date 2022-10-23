057701edabde386f030155d9398570f19846e1af
Alexey Tolkov <TolkovAS@GMail.com>
2022-10-23 13:30:01 +0200

    Add project files.

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Notes.Models
+{
+    internal class Note
+    {
+        public string FileName { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
---
+++ Notes/Views/AboutPage.xaml.cs
@@ -0,0 +1,21 @@
+namespace Notes.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+
+        label.BindingContext = slider;
+        label.SetBinding(Label.RotationProperty, "Value");
+    }
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.About about)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+    }
+}
